ebae9a21c240bad2ce6ea83ce6a9f58bebb9bcdb
ALHAOUIL ABDESSAMAD <152272492+a-alhaouil@users.noreply.github.com>
2024-06-21 09:48:11 +0100

    Add files via upload

---
+++ TR4/2eme_Partie__part1/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {NgForOf} from "@angular/common";
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, NgForOf,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '2eme_Partie_Part1';
+  actions :Array<any> =[
+    {title :"Home", "route":"/home", icon : "house"},
+    {title :"Products", "route":"/products", icon : "search"},
+    {title :"New Product", "route":"/newProduct", icon : "plus-circle"}
+
+  ];
+  currentAction : any
+  setCurrentAction(action: any){
+    this.currentAction = action;
+
+}
+}
