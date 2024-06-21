9299fd3ce768479174e7fce18596fea6a55d631f
ALHAOUIL ABDESSAMAD <152272492+a-alhaouil@users.noreply.github.com>
2024-06-21 19:14:27 +0100

    Add files via upload

---
+++ TR4/2eme_Partie__part3/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '2eme_Partie_Part1';
+  actions :Array<any>=[
+    {title:"Home" , "route":"/home", icon:"house"},
+    {title:"Products" , "route":"/products", icon:"search"},
+    {title:"New Product" , "route":"/newProduct", icon:"safe"}
+  ];
+  currentAction:any
+
+  setCurrentAction(action: any) {
+    this.currentAction=action;
+  }
+}
